e3c671cc4cdb2a59d1302e5be0ea8f47d8a4053c
Mario Rugeles <mrugeles@gmail.com>
2019-02-24 18:18:41 +0000

    One hot encoding genre feature

---
+++ data/process_data.py
@@ -0,0 +1,74 @@
+import sys
+import pandas as pd
+
+def get_encodings(filepath):
+    from encodings.aliases import aliases
+
+    alias_values = set(aliases.values())
+
+    for encoding in set(aliases.values()):
+        try:
+            df=pd.read_csv(filepath, encoding=encoding)
+            print('successful', encoding)
+        except:
+            pass
+
+def load_data(messages_filepath, categories_filepath):
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, left_on='id', right_on='id')
+
+    categories = df['categories'].str.split(';', expand=True)
+    row = categories.iloc[0]
+    category_colnames = list(row.apply(lambda category_value: category_value.split('-')[0]))
+    categories.columns = category_colnames
+    for column in categories:
+        categories[column] = categories[column].astype(str).str.split('-')[0][1]
+        categories[column] = categories[column].astype(int)
+
+    df = df.drop(['categories'], axis = 1)
+    df = pd.concat([df, categories], axis = 1)
+    
+    genres = pd.get_dummies(df[['genre']])
+    df = df.drop(['genre'], axis = 1)
+    df = pd.concat([df, genres], axis = 1)
+    return df
+
+def clean_data(df):
+    return df.drop_duplicates()
+
+
+def save_data(df, database_filename):
+    from sqlalchemy import create_engine
+    engine = create_engine('sqlite:///'+database_filename)
+    df.to_sql('messages', engine, index=False, if_exists='replace')
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+
+        print('Cleaned data saved to database!')
+
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
---
+++ models/train_classifier.py
@@ -0,0 +1,111 @@
+import sys
+import nltk
+import re
+
+import pandas as pd
+
+from sqlalchemy import create_engine
+
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.stem import PorterStemmer
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+def load_data(database_filepath):
+    engine = create_engine('sqlite:///'+database_filepath)
+    df = pd.read_sql('messages', engine)
+    print(df.shape)
+    X = df[['message']].values.flatten()
+    y = df.drop(['id', 'message', 'original'], axis = 1)
+    category_names = list(y.columns.values)
+    y = pd.get_dummies(y)
+    y = y.values
+   
+    
+    return X, y, category_names
+
+
+def tokenize(text):
+    text = text.lower() 
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text) 
+    words = word_tokenize(text)
+    words = [w for w in words if w not in stopwords.words("english")]
+    words = [WordNetLemmatizer().lemmatize(w, pos='v') for w in words]
+    words = stemmed = [PorterStemmer().stem(w) for w in words]
+    return text
+
+
+def build_model():
+    forest = RandomForestClassifier()
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(forest))
+    ])
+
+    return pipeline
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    y_pred = model.predict(X_test)
+    print(classification_report(Y_test, y_pred, target_names=category_names))
+    '''
+    accuracy = accuracy_score(Y_test, y_pred)
+    precision = precision_score(Y_test, y_pred, average = 'micro')
+    recall = recall_score(Y_test, y_pred, average = 'micro')
+    print("accuracy: %f"%accuracy)
+    print("precision: %f"%precision)
+    print("recall: %f"%recall)
+    '''
+
+
+def save_model(model, model_filepath):
+    return True
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        print(category_names)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
