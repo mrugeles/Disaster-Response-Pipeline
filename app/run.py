1c3fa7889f3815fa7080b4635eea7888ebed48d9
Mario Rugeles <mrugeles@gmail.com>
2019-03-02 13:03:20 -0500

    Updating web script

--- app/run.py
+++ app/run.py
@@ -1,6 +1,7 @@
 import json
 import plotly
 import pandas as pd
+import numpy as np
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
@@ -26,23 +27,37 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('messages', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
-
+columns = ['related', 'request', 'offer',
+   'aid_related', 'medical_help', 'medical_products',
+   'search_and_rescue', 'security', 'military', 'child_alone', 'water',
+   'food', 'shelter', 'clothing', 'money', 'missing_people',
+   'refugees', 'death', 'other_aid', 'infrastructure_related',
+   'transport', 'buildings', 'electricity', 'tools', 'hospitals',
+   'shops', 'aid_centers', 'other_infrastructure', 'weather_related',
+   'floods', 'storm', 'fire', 'earthquake', 'cold', 'other_weather',
+   'direct_report', 'genre_direct', 'genre_news', 'genre_social']
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
-    
+
     # extract data needed for visuals
     # TODO: Below is an example - modify to extract data for your own visuals
-    genre_counts = df.groupby('genre').count()['message']
-    genre_names = list(genre_counts.index)
-    
+    genre_direct_counts = df.loc[df['genre_direct'] == 1].shape[0]
+    genre_news_counts = df.loc[df['genre_news'] == 1].shape[0]
+    genre_social_counts = df.loc[df['genre_social'] == 1].shape[0]
+
+    genre_counts = [genre_direct_counts, genre_news_counts, genre_social_counts]
+    genre_names = ['direct', 'news', 'social']
+    #genre_counts = df.groupby('genre').count()['message']
+    #genre_names = list(genre_counts.index)
+
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
@@ -65,11 +80,11 @@ def index():
             }
         }
     ]
-    
+
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
+
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
@@ -78,13 +93,16 @@ def index():
 @app.route('/go')
 def go():
     # save user input in query
-    query = request.args.get('query', '') 
-
+    query = request.args.get('query')
     # use model to predict classification for query
-    classification_labels = model.predict([query])[0]
-    classification_results = dict(zip(df.columns[4:], classification_labels))
+    print("query:")
+    print(query)
+    classification_labels = model.predict([query])
+    print(classification_labels[0])
+    print( model.predict([query]))
+    classification_results = dict(zip(columns, classification_labels[0]))
 
-    # This will render the go.html Please see that file. 
+    # This will render the go.html Please see that file.
     return render_template(
         'go.html',
         query=query,
